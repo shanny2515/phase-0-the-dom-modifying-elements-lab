4202c19fa3a0c0fd54953b349aa6ca094c06abd6
shanny2515 <damarshania05@gmail.com>
2025-06-14 22:20:33 +0300

    modify index.html and index.js and add package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+ newHeader.innerHTML = 'Shanny is the champion'
+ console.log(newHeader)
+ document.body.append(newHeader)
